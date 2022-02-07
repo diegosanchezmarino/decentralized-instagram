2ca4c58c6ea6659f449da991f3264ffd04bed744
Diego Sanchez Mariñ0 <diegosm@Diegos-MacBook-Pro.local>
2022-02-07 10:46:04 -0300

    Updates

---
+++ precache-manifest.b20a8fdfa096423a2927df6dbae6dfb5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49506ab050e22bf8d42",
+    "url": "/decentralized-instagram/static/css/main.5eeadc91.chunk.css"
+  },
+  {
+    "revision": "c49506ab050e22bf8d42",
+    "url": "/decentralized-instagram/static/js/main.c49506ab.chunk.js"
+  },
+  {
+    "revision": "2b474ef177d23b55fc00",
+    "url": "/decentralized-instagram/static/css/1.634b834a.chunk.css"
+  },
+  {
+    "revision": "2b474ef177d23b55fc00",
+    "url": "/decentralized-instagram/static/js/1.2b474ef1.chunk.js"
+  },
+  {
+    "revision": "ea5f8307332803c69620",
+    "url": "/decentralized-instagram/static/js/runtime~main.ea5f8307.js"
+  },
+  {
+    "revision": "c41904a6c860f31389c2b4406fbd6103",
+    "url": "/decentralized-instagram/static/media/logo.c41904a6.png"
+  },
+  {
+    "revision": "76be49c93fd789788f91a9efb61e9d71",
+    "url": "/decentralized-instagram/static/media/new_post_icon.76be49c9.svg"
+  },
+  {
+    "revision": "fd6c2f7eac3d86671c784af20ccef017",
+    "url": "/decentralized-instagram/static/media/images_icon.fd6c2f7e.svg"
+  },
+  {
+    "revision": "240b0ccda2162cb1aa004c14d3c24837",
+    "url": "/decentralized-instagram/static/media/paid_black_24dp.240b0ccd.svg"
+  },
+  {
+    "revision": "83d8a8b2d969f34b0a735fda6d34393c",
+    "url": "/decentralized-instagram/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/decentralized-instagram/precache-manifest.b20a8fdfa096423a2927df6dbae6dfb5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/decentralized-instagram/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
